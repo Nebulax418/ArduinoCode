b3417a4a06f9e9062c0bec3f9c6ec06abd863964
Archisman Nath <61641517+Nebulax418@users.noreply.github.com>
2022-02-17 16:15:27 -0500

    UPDATE CODE.CPP for usage of tinkercad

---
+++ code.cpp
@@ -0,0 +1,51 @@
+const int motorIn1 = 9;
+
+const int motorIn2 = 10;
+
+void setup()
+
+{
+
+  pinMode(motorIn1,OUTPUT);  //initialize the motorIn1 pin as output 
+
+  pinMode(motorIn2,OUTPUT);  //initialize the motorIn2 pin as output 
+
+}
+
+void loop() {
+
+  int a = 10;
+  while(a != 0) {
+    clockwise(200);  //rotate clockwise 
+    delay(100);
+    if(a == 5 || a == 7 || a == 3) {
+       counterclockwise(200);  //rotate counterclockwise
+       delay(100); 
+    }
+    a-=1
+  }
+}
+
+void clockwise(int speed) {
+
+  int half = speed / 2;
+  
+  while(speed != half) {
+    analogWrite(motorIn1, speed);  //set the speed of motor
+    analogWrite(motorIn2, speed);  //stop the motorIn2 pin of motor
+    speed-=10;
+  }
+  
+
+}
+
+void counterclockwise(int Speed) {
+
+  int half = speed / 2;
+  
+  while(speed != half) {
+    analogWrite(motorIn1, 0);  //set the speed of motor
+    analogWrite(motorIn2, speed);  //stop the motorIn2 pin of motor
+    speed-=10;
+  }
+}
